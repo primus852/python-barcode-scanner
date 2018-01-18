7be9b0c6cca4f26f807ed9f645382a600e700de9
primus852 <primus852@users.noreply.github.com>
2018-01-19 00:30:31 +0100

    Initial

---
+++ beep.py
@@ -0,0 +1,27 @@
+import threading
+import winsound
+
+
+class MakeBeep(threading.Thread):
+
+    def __init__(self, sound_type):
+        threading.Thread.__init__(self)
+        self.sound_type = sound_type
+
+    def run(self):
+
+        frequency = 2500  # Default Frequency (Hertz)
+        duration = 200  # Default Duration (ms)
+
+        if self.sound_type == 'success':
+            frequency = 1800
+            duration = 200
+            winsound.Beep(frequency, duration)
+        elif self.sound_type == 'fail':
+            frequency = 2800
+            duration = 100
+            winsound.Beep(frequency, duration)
+            winsound.Beep(frequency, duration)
+            winsound.Beep(frequency, duration)
+        else:
+            winsound.Beep(frequency, duration)
